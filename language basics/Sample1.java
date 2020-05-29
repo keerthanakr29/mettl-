ce6acb5d8d528acd3ef4496d6e106121d9e9187b
keerthanakr29 <66018944+keerthanakr29@users.noreply.github.com>
2020-05-29 19:22:14 +0530

    language basics

---
+++ language basics/Sample1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Sample1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome "+args[0]);
+	}
+}
---
+++ language basics/Sample2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Sample2
+{
+	public static void main(String args[])
+	{
+		System.out.println("The sum of 10 and 20 is "+args[0]);
+	}
+}
