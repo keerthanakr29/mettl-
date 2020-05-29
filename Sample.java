3ff78b37d0e6eea5ed6b01c385861eac96e0f6e3
keerthanakr29 <66018944+keerthanakr29@users.noreply.github.com>
2020-05-29 11:43:58 +0530

    language basics

---
+++ Sample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Sample
+{
+	public static void main(String args[])
+	{
+		System.out.println("wipro "+args[0]+" Bangalore");
+		System.out.println("ABC "+args[0]+" Mumbai");
+	}
+}
